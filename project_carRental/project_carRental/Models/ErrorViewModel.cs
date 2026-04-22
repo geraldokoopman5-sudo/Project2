ee9a5f0689927447e60ffb7e7e86e7f1a997b0d3
23muneebryklief-design <23muneebryklief@gmail.com>
2026-04-22 15:21:52 +0200

    Add files via upload
    back-end_asp.net

---
+++ project_carRental/project_carRental/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace project_carRental.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
